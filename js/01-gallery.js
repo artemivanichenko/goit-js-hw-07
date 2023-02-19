5252a26f024f663a7f4a70655f9866950f24b2af
Artem <ivanichenko.ua@gmail.com>
2023-02-19 13:11:10 +0200

    completed 7 dz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const createGallerycontainer = document.querySelector(".gallery");
+const newGallery = createGalleryForms(galleryItems);
+
+createGallerycontainer.insertAdjacentHTML("beforeend", newGallery);
+createGallerycontainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryForms(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        data-source="${original}"
+        alt "${description}" 
+        >
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img  src="${evt.target.src}" >
+  `);
+
+  instance.show(evt);
+
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import { galleryItems } from "./gallery-items.js";
+
+const createGallerycontainer = document.querySelector(".gallery");
+const newGallery = createGalleryForms(galleryItems);
+
+createGallerycontainer.insertAdjacentHTML("beforeend", newGallery);
+createGallerycontainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryForms(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        data-source="${original}"
+        alt "${description}" 
+        >
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // const instance = basicLightbox.create(`
+  //     <img src="${evt.target.src}">
+  // `);
+
+  //   instance.show(evt);
+
+  //   document.addEventListener("keydown", (evt) => {
+  //     if (evt.key === "Escape") {
+  //       instance.close();
+  //     }
+  //   });
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionData: "alt",
+});
